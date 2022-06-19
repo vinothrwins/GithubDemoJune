cac4e6e454c613f99765eb3956dfd8b872f8c40b
PCNAME <PCNAME@DESKTOP-IB6GJ2Q>
2022-06-19 18:52:08 +0530

    Added the sysout msg

--- GithubDemoJavaProject/src/com/nxtgenai/demo/WelcomeUser.java
+++ GithubDemoJavaProject/src/com/nxtgenai/demo/WelcomeUser.java
@@ -3,8 +3,13 @@ package com.nxtgenai.demo;
 public class WelcomeUser {
 
 	public static void main(String[] args) {
+		
 		// Code added by Vinoth 
 		System.out.println("Welcome to Github Session");
+		
+		
+		// Coded added by Pranav
+		System.out.println("Welcome to Automation");
 	}
 
 }
